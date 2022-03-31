43d2b790656316ad9fcdbafaa78a9a9f6b29ba6c
Sumin Kim <cm14121453@gmail.com>
2022-03-31 21:46:08 +0900

    0329

--- C/test.cpp
+++ C/test.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 int main() {
 	char a;
+
 	cout << "문자를 입력하시오. ";
 	cin >> a;
-	if (a == 'O') cout << "정답입니다." << endl << "프로그램을 종료합니다." << endl;
-	else cout << "프로그램을 종료합니다." << endl;
+
+	if (a == 'O') cout << "정답입니다.\n프로그램을 종료합니다.\n";
+	else cout << "프로그램을 종료합니다.\n";
+
 	system("pause");
 }
